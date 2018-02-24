37a4e63c498ed6b5f1fc789db99e7f82b64487f7
ziggysauce <dtn3423@gmail.com>
2018-02-24 15:35:18 -0500

    General styling for all components

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,7 +6,13 @@ export default class Intro extends Component {
   render() {
 		return (
 			<div className="intro">
-        This is from Intro!
+				<div className="intro-left">
+					<h2>Hello.</h2>
+					<p>My name is Dan Nguyen.</p>
+				</div>
+				<div className="intro-right">
+					<p>I'm a passionate front-end web developer based in DC.</p>
+				</div>
 			</div>
 		);
   }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,16 +5,14 @@ export default class Navbar extends Component {
 	
   render() {
 		return (
-			<div className="navbar">
-				<a href="#" className="navbar__logo">
-					dtn.
-				</a>
+			<header className="navbar">
+				<a href="#" className="navbar__logo">dtn.</a>
 				<ul className="navbar__list">
 					<li className="navbar__link"><a href="#">About</a></li>
 					<li className="navbar__link"><a href="#">Projects</a></li>
 					<li className="navbar__link"><a href="#">Contact</a></li>
 				</ul>
-			</div>
+			</header>
 		);
   }
 }
