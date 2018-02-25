36d0a770b7af85aac732d5683048807b010c7ec9
ziggysauce <dtn3423@gmail.com>
2018-02-25 11:46:08 -0500

    update resume file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,7 +21,7 @@ export default class Footer extends Component {
 						<li><a className="footer__link" href="https://github.com/ziggysauce" target="_blank"><i className="fab fa-github"></i></a></li>
 						<li><a className="footer__link" href="https://codepen.io/ziggysauce/" target="_blank"><i className="fab fa-codepen"></i></a></li>
 						<li><a className="footer__link" href="https://medium.com/@ziggysauce" target="_blank"><i className="fab fa-medium-m"></i></a></li>
-						<li><a className="footer__link" href="https://drive.google.com/file/d/1VdoxUOacJ6HNdt8ZyIqjZT9s1QxdWzi1/view?usp=sharing" target="_blank"><i className="fas fa-file-pdf"></i></a></li>
+						<li><a className="footer__link" href="https://drive.google.com/file/d/1svpj_x4qLEkNHlha-2QQC9xEiH0nfzx3/view?usp=sharing" target="_blank"><i className="fas fa-file-pdf"></i></a></li>
 					</ul>
 				</div>
 			</footer>
