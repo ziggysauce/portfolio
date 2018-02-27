d44c68f241450403a7014594b79a78507e4f0f87
ziggysauce <dtn3423@gmail.com>
2018-02-27 15:26:21 -0500

    Update projecs, fix small style changes

--- src/api/Projects.jsx
+++ src/api/Projects.jsx
@@ -6,39 +6,23 @@ module.exports = {
       description: "A front-end developer focused new tab extension for your web browser with features including sticky notes, quicklinks, a color picker, and more.",
       live: "https://chingu-coders.github.io/Voyage2-Turtles-02/TabLandingPage/index.html",
       repo: "https://github.com/chingu-coders/Voyage2-Turtles-02",
-      img: "https://s19.postimg.org/vjfs8waar/Dev_Tab_Browser.png"
+      img: "https://s19.postimg.org/gdhlas4er/Dev_Tab_showcase.png"
     },
     {
-      name: "devtab",
-      title: "DevTab",
-      description: "A front-end developer focused new tab extension for your web browser with features including sticky notes, quicklinks, a color picker, and more.",
-      live: "https://chingu-coders.github.io/Voyage2-Turtles-02/TabLandingPage/index.html",
-      repo: "https://github.com/chingu-coders/Voyage2-Turtles-02",
-      img: "https://s19.postimg.org/vjfs8waar/Dev_Tab_Browser.png"
+      name: "anjali",
+      title: "Artist Webpage",
+      description: "A portfolio webpage for a local D.C. music artist.",
+      live: "http://anjalitaneja.com/",
+      repo: "https://github.com/ziggysauce/anjali_taneja",
+      img: "https://s19.postimg.org/btnfpmchv/anjali_showcase.png"
     },
     {
-      name: "devtab",
-      title: "DevTab",
-      description: "A front-end developer focused new tab extension for your web browser with features including sticky notes, quicklinks, a color picker, and more.",
-      live: "https://chingu-coders.github.io/Voyage2-Turtles-02/TabLandingPage/index.html",
-      repo: "https://github.com/chingu-coders/Voyage2-Turtles-02",
-      img: "https://s19.postimg.org/vjfs8waar/Dev_Tab_Browser.png"
-    },
-    {
-      name: "devtab",
-      title: "DevTab",
-      description: "A front-end developer focused new tab extension for your web browser with features including sticky notes, quicklinks, a color picker, and more.",
-      live: "https://chingu-coders.github.io/Voyage2-Turtles-02/TabLandingPage/index.html",
-      repo: "https://github.com/chingu-coders/Voyage2-Turtles-02",
-      img: "https://s19.postimg.org/vjfs8waar/Dev_Tab_Browser.png"
-    },
-    {
-      name: "devtab",
-      title: "DevTab",
-      description: "A front-end developer focused new tab extension for your web browser with features including sticky notes, quicklinks, a color picker, and more.",
-      live: "https://chingu-coders.github.io/Voyage2-Turtles-02/TabLandingPage/index.html",
-      repo: "https://github.com/chingu-coders/Voyage2-Turtles-02",
-      img: "https://s19.postimg.org/vjfs8waar/Dev_Tab_Browser.png"
+      name: "duongnewsletter",
+      title: "Duong's 11 Newsletter",
+      description: "A webpage to showcase/archive each newsletter submission.",
+      live: "#",
+      repo: "https://github.com/ziggysauce/duongs_11",
+      img: "https://s19.postimg.org/c6jf9ak2r/duongs11_showcase.png"
     },
   ]
 };
