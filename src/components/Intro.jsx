582585db70a65b8a598591d3c5d2c9caec7a9a2b
ziggysauce <dtn3423@gmail.com>
2018-03-06 12:16:25 -0500

    Update intro animation

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import Fade from 'react-reveal/Fade';
 
 const Intro = (props) => {
 	return (
 		<div id="intro" className="intro">
-			<Fade clear>
-				<div className="intro-left">
-					<h2>Hello.</h2>
-					<p>My name is Daniel Nguyen.</p>
-				</div>
-			</Fade>
-			<Fade clear>
-				<div className="intro-right">
-					<p>I'm a passionate web developer based in DC. Interested in <a onClick={(e) => props.scrollTo(e.target.getAttribute('href'))} href="#footer" className="paragraph-link underline">connecting?</a></p>
-				</div>
-			</Fade>
+			<div className="intro-left fade-in">
+				<h2>Hello.</h2>
+				<p>My name is Daniel Nguyen.</p>
+			</div>
+			<div className="intro-right fade-in">
+				<p>I'm a passionate web developer based in DC. Interested in <a onClick={(e) => props.scrollTo(e.target.getAttribute('href'))} href="#footer" className="paragraph-link underline">connecting?</a></p>
+			</div>
 		</div>
 	);
 }
