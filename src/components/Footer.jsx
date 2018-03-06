b00060883fbc7a15de5ea8a1386475dc8515d5ad
ziggysauce <dtn3423@gmail.com>
2018-03-06 11:09:18 -0500

    Update resume

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = (props) => {
 						<li><a className="footer__link" href="https://github.com/ziggysauce" target="_blank"><i className="fab fa-github"></i></a></li>
 						<li><a className="footer__link" href="https://codepen.io/ziggysauce/" target="_blank"><i className="fab fa-codepen"></i></a></li>
 						<li><a className="footer__link" href="https://medium.com/@ziggysauce" target="_blank"><i className="fab fa-medium-m"></i></a></li>
-						<li><a className="footer__link" href="https://drive.google.com/file/d/1svpj_x4qLEkNHlha-2QQC9xEiH0nfzx3/view?usp=sharing" target="_blank"><i className="fas fa-file-pdf"></i></a></li>
+						<li><a className="footer__link" href="https://drive.google.com/file/d/1VdoxUOacJ6HNdt8ZyIqjZT9s1QxdWzi1/view" target="_blank"><i className="fas fa-file-pdf"></i></a></li>
 					</ul>
 				</div>
 			</Fade>
