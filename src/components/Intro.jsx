ce058cdec012df7b6010e8d45b7ce01d67e9757e
ziggysauce <dtn3423@gmail.com>
2018-03-07 15:12:23 -0500

    Update intro bg and button

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 const Intro = (props) => {
 	return (
 		<div id="intro" className="intro">
+			<div className="intro-bg"></div>
 			<div className="intro-left fade-in">
 				<h2>Hello.</h2>
 				<p>My name is Daniel Nguyen.</p>
 			</div>
 			<div className="intro-right fade-in">
-				<p>I'm a passionate web developer based in DC. Interested in <a onClick={(e) => props.scrollTo(e.target.getAttribute('href'))} href="#footer" className="paragraph-link underline">connecting?</a></p>
+				<p>I'm a passionate web developer based in DC.</p>
+				<a onClick={(e) => props.scrollTo(e.target.getAttribute('href'))} href="#footer" className="intro-cta">Let's connect</a>
 			</div>
 		</div>
 	);
