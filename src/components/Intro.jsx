7d7a36e2f351c6890da6ac62b0b1a42da20a9cf6
ziggysauce <dtn3423@gmail.com>
2018-03-26 13:04:42 -0500

    Rework intro bg

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Intro = (props) => {
 	return (
 		<div id="intro" className="intro">
-			<div className="intro-bg"></div>
+			<img className="intro-pic" src="https://images.pexels.com/photos/194094/pexels-photo-194094.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb" alt="background image with boats"/>
 			<div className="intro-left">
 				<h2>Hello.</h2>
 				<p>My name is Daniel Nguyen.</p>
