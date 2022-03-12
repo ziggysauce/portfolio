b3f5df2a1aef711ec10adc6b07bba31efb52abb8
ziggysauce <dtn3423@gmail.com>
2022-03-12 02:12:41 -0500

    declare react-face

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
+/// <reference types="react-reveal/Fade" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
